d0379a7ba71afb9bfc8c66fef1ee925b55cc9b2f
Roma <nrm0977@gmail.com>
2014-12-23 00:35:41 +0200

    0.0.2

--- BankSheets/src/start/Start.java
+++ BankSheets/src/start/Start.java
@@ -1,10 +1,45 @@
+/**
+ * 
+ */
 package start;
 
-public class Start {
+import java.awt.HeadlessException;
 
+import javax.swing.JFrame;
+
+/**
+ * @author Roman
+ *
+ */
+public class Start extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("www");
+		
+		new Start("Формування списків в банк");
 	}
+
+	/**
+	 * @param title
+	 * @throws HeadlessException
+	 */
+	public Start(String title) throws HeadlessException {
+		super(title);
+		setSize(400, 400);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+
+	
 
 }
